5907b7e8d0ae4aa2ef4c5cbb18b35c6d5d2c34d8
liviu-o <li.viu@mail.com>
2023-06-02 16:35:13 +0100

    Filter components based on year

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,25 @@
-import { useState } from 'react';
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    const [title, setTitle] = useState(props.title);
+  const [title, setTitle] = useState(props.title);
 
-    const handleClick = () => {   
-        setTitle('Updated');     
-        console.log(title);
-    }   
+  const handleClick = () => {
+    setTitle("Updated");
+  };
 
-    return (
-        <Card className='expense-item'>
-           <ExpenseDate date={props.date} />
-            <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-            <button onClick={handleClick}>Change Title</button>
-        </Card>
-    )
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={handleClick}>Change Title</button>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
